bc33cf6d247584518c225409ee2bd75c95cfd13f
OnkarabileMasoko <156708064+OnkarabileMasoko@users.noreply.github.com>
2024-07-31 16:43:00 +0200

    Delete masoko-store/src/components directory

--- masoko-store/src/components/Store.js
+++
@@ -1,6 +0,0 @@
-// src/stores.js
-import { writable } from 'svelte/store';
-
-export const selectedCategory = writable('');
-export const searchQuery = writable('');
-export const sortOption = writable('default');
